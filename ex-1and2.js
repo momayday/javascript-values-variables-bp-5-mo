6806f7cd09a4c900db474ce85755c5b8fd88edd5
Ratthapol Limtaweesomkiat <ratthapollimtaweesomkiat@Ratthapols-MacBook-Air.local>
2024-08-16 17:49:01 +0700

    Mo : assign send javascript newest

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,25 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let surname = "Karter";
+let lastname ="Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+console.log(surname);
+console.log(lastname);
+console.log(address);
 
+let restaurantName = "Chef's Table by Chef James";
+let valueAmount = 15000;
+let restaurantAmount = 1000;
+console.log(restaurantName);
+console.log(valueAmount);
+console.log(restaurantAmount);
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+console.log(surname);
+console.log(lastname);
+console.log(address);
+valueAmount = 5000;
+restaurantAmount = 2500;
+console.log(valueAmount);
+console.log(restaurantAmount);
